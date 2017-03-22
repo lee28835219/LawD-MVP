0b483c30059ac6b5930fba8211fa52066b662341
leebirth <leebirth@gmail.com>
2017-03-22 22:57:11 +0900

    Initial Commit

---
+++ TestGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestGenerator
+//
+//  Created by Master Builder on 2017. 3. 22..
+//  Copyright © 2017년 MasterBuilder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
