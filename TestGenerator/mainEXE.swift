5b0904b5cd18e7d61634801e91b4f698f1feba2f
Masterbuilder <masterbuilder2030@gmail.com>
2023-09-19 23:41:33 +0900

    9A8B807을 디코딩 하는데 성공했습니다.

--- TestGenerator/mainEXE.swift
+++ TestGenerator/mainEXE.swift
@@ -13,6 +13,8 @@ class mainEXE {
         if toExecute {
             // 아래에서 콘솔에서 실행할 액션를 정의함.
 //            print(QuestionData.saveQueD1().creationDate.myDateStirng)
+            let queD1 = QuestionData.loadLatestQueD1()
+            print(queD1.id)
         }
     }
     
